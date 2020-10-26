1808d77d02a3d22a2bf7c5ddc72d38acf39924c1
Joshua Colley <joshua@Joshuas-MacBook-Pro.local>
2020-10-26 17:39:44 +0000

    Create Hospital Model template

--- Sensyne Hospitals/Sensyne Hospitals/Shared/Models/Hospital.swift
+++ Sensyne Hospitals/Sensyne Hospitals/Shared/Models/Hospital.swift
@@ -5,6 +5,27 @@
 //  Created by Joshua Colley on 26/10/2020.
 //
 
-class Hospital: Codable {
-    
+struct Hospital {
+    let organisationId: Int
+    let organisationCode: String
+    let organisationType: String
+    let organisationName: String
+
+    let subtype: String
+    let sector: String
+    let organisationStatus: String
+    let isPimsManaged: Bool
+    let parentODSCode: String
+
+    let address1: String
+    let address2: String
+    let address3: String
+    let city: String
+    let county: String
+    let postcode: String
+    let latitude: Double
+    let longitude: Double
+    let phoneNumber: String
+    let website: String
+    let fax: String
 }
---
+++ Sensyne Hospitals/Sensyne Hospitals/Shared/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  Sensyne Hospitals
+//
+//  Created by Joshua Colley on 26/10/2020.
+//
+
+// MARK: - NetworkManagerProtocol
+protocol NetworkManagerProtocol {
+}
+
+// MARK: - NetworkManager
+class NetworkManager: NetworkManagerProtocol {
+    
+}
